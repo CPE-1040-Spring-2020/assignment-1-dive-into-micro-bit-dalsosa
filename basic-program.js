8fa18f197f6eca7472792d2430c661e9fa65fbef
dalsosa <60205412+dalsosa@users.noreply.github.com>
2020-01-22 19:39:29 -0700

    Update basic-program.js

--- basic-program.js
+++ basic-program.js
@@ -1,5 +1,16 @@
+let iconheart: boolean = true
+
+input.onButtonPressed(Button.A, function () {
+    iconheart = !iconheart
+})
+
+
 basic.forever(function () {
-    basic.showIcon(IconNames.Heart)
+    if (iconheart) {
+        basic.showIcon(IconNames.Heart)
+    } else {
+        basic.showIcon(IconNames.Butterfly)
+    }
     basic.pause(100)
     basic.clearScreen()
 })
